622d275b3943ffdcbcbd1a5102e5c3c5868ca2fa
grcamposdev <grcamposdev@gmail.com>
2021-12-30 11:56:47 +0100

    Commit inicial

---
+++ src/main/java/com/grcamposdev/appcheck/AppcheckApplication.java
@@ -0,0 +1,13 @@
+package com.grcamposdev.appcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grcamposdev/appcheck/AppcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grcamposdev.appcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
